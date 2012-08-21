0b87ae87f2b5dca511293a8ebcab98f9f211b504
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-08-21 14:38:34 +0000

    common async task to use in apps.

---
+++ src/eu/trentorise/smartcampus/android/common/SCAsyncTask.java
@@ -0,0 +1,62 @@
+package eu.trentorise.smartcampus.android.common;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+public class SCAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+
+	private ProgressDialog progress = null;
+
+	private Activity activity;
+	private SCAsyncTaskProcessor<Params,Result> processor;
+	private enum STATUS {OK, SECURITY, FAILURE};
+	private Exception error;
+	private STATUS status = STATUS.OK;
+	
+	public SCAsyncTask(Activity activity, SCAsyncTaskProcessor<Params,Result> processor) {
+		super();
+		this.activity = activity;
+		this.processor = processor;
+	}
+
+	@Override
+	protected Result doInBackground(Params... params) {
+		try {
+			return processor.performAction(params);
+		} catch (eu.trentorise.smartcampus.protocolcarrier.exceptions.SecurityException e) {
+			status = STATUS.SECURITY;
+		} catch (Exception e) {
+			error = e;
+			status = STATUS.FAILURE;
+		}
+		
+		return null;
+	}
+	
+	@Override
+	protected void onPostExecute(Result result) {
+		if (progress != null) progress.dismiss();
+		if (status == STATUS.OK) processor.handleResult(result);
+		else if (status == STATUS.SECURITY) {
+			processor.handleSecurityError();
+		}
+		else {
+			processor.handleFailure(error);
+		}
+	}
+
+	@Override
+	protected void onPreExecute() {
+		progress  = ProgressDialog.show(activity, "", "Loading. Please wait...", true);
+		super.onPreExecute();
+	}
+
+	public interface SCAsyncTaskProcessor<Params,Result> {
+		Result performAction(Params ...params) throws eu.trentorise.smartcampus.protocolcarrier.exceptions.SecurityException, Exception;
+		void handleResult(Result result);
+		void handleFailure(Exception e);
+		void handleSecurityError();
+	}
+
+}
