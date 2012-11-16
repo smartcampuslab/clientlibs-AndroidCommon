7554cc7e560d154866f3408cd3893ca49cb69bdc
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-11-16 17:03:03 +0000

    correct handling of secure addresses; correct vas-dev URL

--- src/eu/trentorise/smartcampus/android/common/GlobalConfig.java
+++ src/eu/trentorise/smartcampus/android/common/GlobalConfig.java
@@ -2,5 +2,5 @@ package eu.trentorise.smartcampus.android.common;
 
 public class GlobalConfig {
 
-	public static final String APP_HOST = "https://vas-dev.sc.trentorise.eu";
+	public static final String APP_HOST = "https://vas-dev.smartcampuslab.it";
 }
