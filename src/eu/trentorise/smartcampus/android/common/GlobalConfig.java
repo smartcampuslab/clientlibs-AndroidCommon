4832ba7620733ed02f796005254c23190a362d7d
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-12-05 17:49:30 +0000

    Use shared preferences to store app url instead of hardcoded constant.

--- src/eu/trentorise/smartcampus/android/common/GlobalConfig.java
+++ src/eu/trentorise/smartcampus/android/common/GlobalConfig.java
@@ -1,6 +1,58 @@
 package eu.trentorise.smartcampus.android.common;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager.NameNotFoundException;
+import eu.trentorise.smartcampus.protocolcarrier.exceptions.ProtocolException;
+
 public class GlobalConfig {
 
-	public static final String APP_HOST = "https://vas-dev.smartcampuslab.it";
+//	public static final String APP_HOST = "https://vas.smartcampuslab.it";
+	
+	private static final String P_APP_BASE_URL= "APP_BASE_URL";
+	// Shared package path
+	private static final String SHARED_PACKAGE = "eu.trentorise.smartcampus.launcher";
+	// Name for preferences
+	private static final String COMMON_PREF = "COMMON_PREF";
+	// Access mode (private to application and other ones with same Shared UID)
+	private static final int ACCESS = Context.MODE_PRIVATE|Context.CONTEXT_RESTRICTED;
+
+	private static final String DEF_APP_HOST = "https://vas-dev.smartcampuslab.it";
+	private static String appUrl = null;
+	
+	public static void setAppUrl(Context ctx, String url) throws NameNotFoundException {
+		assert url != null;
+		SharedPreferences prefs = getPrefs(ctx);
+		Editor edit = prefs.edit();
+		edit.putString(P_APP_BASE_URL, url);
+		edit.commit();
+		appUrl = null;
+	}
+	
+	public static String getAppUrl(Context ctx) throws ProtocolException {
+		if (appUrl == null) {
+			SharedPreferences prefs;
+			try {
+				prefs = getPrefs(ctx);
+				appUrl = prefs.getString(P_APP_BASE_URL, null);
+				// tihs is temporary: the base url should always be set
+				if (appUrl == null) {
+					setAppUrl(ctx, DEF_APP_HOST);
+					prefs = getPrefs(ctx);
+					appUrl = prefs.getString(P_APP_BASE_URL, null);
+				}
+			} catch (NameNotFoundException e) {
+				throw new ProtocolException("No app url provided");
+			}
+
+		}
+		return appUrl;
+	}
+
+	private static SharedPreferences getPrefs(Context context) throws NameNotFoundException {
+		Context sharedContext = context.createPackageContext(SHARED_PACKAGE, ACCESS);
+		return sharedContext.getSharedPreferences(COMMON_PREF, ACCESS);
+	}
+
 }
