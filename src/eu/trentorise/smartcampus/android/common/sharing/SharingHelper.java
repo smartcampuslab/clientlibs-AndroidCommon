9eb4151444a5ed20795b5d58ed0eb2c66c58724e
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-01-17 16:04:42 +0000

    share operation name corrected

--- src/eu/trentorise/smartcampus/android/common/sharing/SharingHelper.java
+++ src/eu/trentorise/smartcampus/android/common/sharing/SharingHelper.java
@@ -21,7 +21,7 @@ import eu.trentorise.smartcampus.android.common.AppHelper;
 
 public class SharingHelper {
 
-	public static void follow(Activity ctx, ShareEntityObject obj) {
+	public static void share(Activity ctx, ShareEntityObject obj) {
 		Intent intent = new Intent();
 		intent.setAction(ctx.getString(eu.trentorise.smartcampus.android.common.R.string.share_intent_action));
 		intent.putExtra(ctx.getString(eu.trentorise.smartcampus.android.common.R.string.share_entity_arg_entity), obj);
