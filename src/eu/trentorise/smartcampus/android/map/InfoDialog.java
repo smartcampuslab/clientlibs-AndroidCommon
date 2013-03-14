d33f695b6960f9413a3f154a3f76b97e755080c4
m.chini <m.chini@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-03-14 17:47:19 +0000

    Add Dialog box when a user select an address directly from the map

---
+++ src/eu/trentorise/smartcampus/android/map/InfoDialog.java
@@ -0,0 +1,67 @@
+package eu.trentorise.smartcampus.android.map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Address;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+import eu.trentorise.smartcampus.android.common.R;
+
+
+public class InfoDialog extends SherlockDialogFragment {
+	private Address address;
+	private Activity activity;
+	public final static int RESULT_SELECTED = 10;
+	
+	public InfoDialog(Activity activity, Address o) {
+		this.address = o;
+		this.activity = activity;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		getDialog().setTitle(R.string.selected_address);
+		return inflater.inflate(R.layout.infoaddressdialog, container, false);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		TextView msg = (TextView) getDialog().findViewById(R.id.mapdialog_msg);
+		if (address != null)
+			msg.setText(address.getAddressLine(0));
+		msg.setMovementMethod(new ScrollingMovementMethod());
+		Button b = (Button) getDialog().findViewById(R.id.mapdialog_cancel);
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				getDialog().dismiss();
+			}
+		});
+		b = (Button) getDialog().findViewById(R.id.mapdialog_ok);
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent data = new Intent();
+				data.putExtra("address", address);
+				data.putExtra("field", activity.getIntent().getExtras().getString("field"));
+				activity.setResult(RESULT_SELECTED, data);
+				activity.finish();
+				getDialog().dismiss();
+			}
+		});
+
+	}
+}
