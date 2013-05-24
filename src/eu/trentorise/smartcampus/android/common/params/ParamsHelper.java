b70be2806090caccc879679bb3d34f797198e06f
o.zambotti <o.zambotti@trentorise.eu>
2013-05-24 16:12:19 +0200

    ParamsHelper simplified

--- src/eu/trentorise/smartcampus/android/common/params/ParamsHelper.java
+++ src/eu/trentorise/smartcampus/android/common/params/ParamsHelper.java
@@ -8,83 +8,50 @@ import java.util.Map;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.util.Log;
 import eu.trentorise.smartcampus.android.common.Utils;
 
-public  class ParamsHelper {
+public class ParamsHelper {
 
-//private static final String FILE_NAME = "params.json";
-private Map<Object,Object> paramsAsset;
-private static Context mContext;
-private static ParamsHelper instance = null;
-private static String filename;
-
-
-public static void init(Context mContext, String file_name) {
-	if (instance == null){
-		instance = new ParamsHelper(mContext,file_name);
-		filename = file_name;
-	}
-}
-protected ParamsHelper(Context mContext, String file_name) {
-	super();
-	this.mContext = mContext;
-	loadParamsFromFile(file_name);
-}
-
-private void loadParamsFromFile(String file_name) {
-	AssetManager assetManager = mContext.getResources().getAssets();
-	try {
-		InputStream in = assetManager.open(file_name);
-		String jsonParams = getStringFromInputStream(in);
-		this.paramsAsset = Utils.convertJSONToObject(jsonParams, Map.class);
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-}
-
-private static String getStringFromInputStream(InputStream is) {
-	 
-	BufferedReader br = null;
-	StringBuilder sb = new StringBuilder();
-
-	String line;
-	try {
-
-		br = new BufferedReader(new InputStreamReader(is));
-		while ((line = br.readLine()) != null) {
-			sb.append(line);
+	public static <T> T load(Context mContext, String assetFilename, Class<T> clazz) {
+		AssetManager assetManager = mContext.getResources().getAssets();
+		try {
+			InputStream in = assetManager.open(assetFilename);
+			String jsonParams = getStringFromInputStream(in);
+			return Utils.convertJSONToObject(jsonParams, clazz);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
 		}
+	}
 
-	} catch (IOException e) {
-		e.printStackTrace();
-	} finally {
-		if (br != null) {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+	@SuppressWarnings("unchecked")
+	public static Map<Object, Object> load(Context mContext, String assetFilename) {
+		return load(mContext, assetFilename, Map.class);
+	}
+
+	private static String getStringFromInputStream(InputStream is) {
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+		String line;
+
+		try {
+			br = new BufferedReader(new InputStreamReader(is));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+
+		return sb.toString();
 	}
-
-	return sb.toString();
-
-}
-private static ParamsHelper getInstance()  {
-	if (instance == null)
-		new ParamsHelper(mContext,filename);
-	return instance;
-}
-
-public static Map<Object, Object> getParamsAsset() {
-		return getInstance().paramsAsset;
-	
-}
-public static void setParamsAsset(Map<Object, Object> paramsAsset) {
-		getInstance().paramsAsset = paramsAsset;
-
-}
-
 
 }
